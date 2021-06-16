8b13edd1f870a3742a303ac0ec8b647863fec21b
Pedro Poglia <pedro.poglia@atua-suporte.1>
2021-06-16 14:40:03 -0300

    Criaçao func,  e reiniciaJogo
    reinicia cronometro, recebe o retorno do timer que serve de param p/ o  pararCronometro agora separada em uma funcao, ja a func do dolar, abreviaçao de dolar(documento.ready(funcaon())responsavel por recarregar as funcoes como um construtor, tb foi implementado a disable do botao while o timer < 1  para evitar bugs

--- public/js/main.js
+++ public/js/main.js
@@ -1,45 +1,87 @@
-var frase = $('.frase').text();
-var numeroPalavras = frase.split(" ").length;
-var tamanhoFrase = $('#tamanho-frase')
-
-tamanhoFrase.text(numeroPalavras);
-
+var tempo = $('#tempo-digitacao');
+const tempoInicial = $('#tempo-digitacao').text();
 var campo = $(".campo-de-digitacao");
+var botaoReiniciar = $("#botao-reiniciar");
 
-
-campo.on("input", atualizaDados);
-campo.one("focus", iniciaContagemRegressiva);
-
-function atualizaDados(){
-
-
-    var conteudo = campo.val();
-
-    var qtdPalavras = conteudo.split(/\S+/).length -1;
-    $("#cont-palavra").text(qtdPalavras);
-
-    var qtdCaracteres = conteudo.length;
-    $('#cont-caracter').text(qtdCaracteres);
+$(function() {
+    console.log("Pagina carregada");
+    reiniciaJogo();
+    atualizaTamanhoFrase();
+    atualizaDados();
+    iniciaCronometro();
     
-};
+    botaoReiniciar.click(reiniciaJogo);
+    
+    
+});
 
+function atualizaTamanhoFrase(){
 
-function iniciaContagemRegressiva(){
-
-    var tempo = $('#tempo-digitacao');
-    var contador = $('#tempo-digitacao').text(); // vale de 3
-
-
-    const timer = setInterval(function(){
-        contador--;
-        tempo.text(contador); 
-        if(contador < 1 ){
-            campo.attr("disabled", true);
-            
-            clearInterval(timer);
-        }
-
-    },1000)
+    var frase = $('.frase').text();
+    var numeroPalavras = frase.split(" ").length;
+    var tamanhoFrase = $('#tamanho-frase')
+    tamanhoFrase.text(numeroPalavras);
 }
 
 
+
+function atualizaDados(){
+
+    campo.on("input", function(){
+
+        var conteudo = campo.val();
+
+        var qtdPalavras = conteudo.split(/\S+/).length -1;
+        $("#cont-palavra").text(qtdPalavras);
+
+        var qtdCaracteres = conteudo.length;
+        $('#cont-caracter').text(qtdCaracteres);
+    });
+};
+
+
+function iniciaCronometro(){
+
+
+    campo.one("focus", function(){
+        botaoReiniciar.attr("disabled", true);
+
+        var contador = $('#tempo-digitacao').text(); // vale de 3
+        
+        const timer = setInterval(function(){
+            contador--; 
+            tempo.text(contador); // coloca o contador dentro da tag
+            if(contador < 1 ){
+
+                paraCronometro(timer);
+                botaoReiniciar.attr("disabled", false);
+            }
+
+        },1000)
+
+    });
+}
+
+
+function reiniciaJogo(timer){
+    
+        paraCronometro(timer);
+        // libera o campo de texto novamente
+        campo.attr("disabled", false);   
+        campo.val(""); //zera o valor 
+
+        // reseta o contador
+        tempo.text(tempoInicial);
+
+        //zera caracteres e palavras
+        $("#cont-palavra").text('0');
+        $('#cont-caracter').text('0');
+
+        iniciaCronometro();
+}
+
+function paraCronometro(timer){
+
+    campo.attr("disabled", true); //desabilita o campo
+    clearInterval(timer); // trava o cronometro
+}
