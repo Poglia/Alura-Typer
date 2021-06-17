9078adc05904b115e7d0ca12c4c80ab6390ee31a
Pedro Poglia <pedro.poglia@atua-suporte.1>
2021-06-17 15:31:55 -0300

    Organizando o projeto
    Criando o placar.js para armazenar as funçoes que envolvem o placar do jogo

--- public/js/main.js
+++ public/js/main.js
@@ -40,18 +40,6 @@ function atualizaDados(){
     });
 };
 
-function inserePlacar(){
-
-    var corpoTabela = $(".placar").find("tbody");
-    var numPalavras = $("#cont-palavra").text();
-    var usuario = "Pedro";
-
-    var linha = novaLinha(usuario,numPalavras);
-    
-    linha.find(".botao-remover").click(removeLinha);
-    corpoTabela.prepend(linha);
-
-}
 
 function inicializaMarcadores(){
 
@@ -73,10 +61,8 @@ function inicializaMarcadores(){
     }
    
 
-});
+    });
 }
-
-
 function iniciaCronometro(){
 
    
@@ -100,32 +86,6 @@ function iniciaCronometro(){
 
     });
 }
-
-function novaLinha(usuario, palavras){
-
-    var linha = $("<tr>");
-    var colunaUsuario = $("<td>").text(usuario);
-    var colunaPalavras = $("<td>").text(palavras);
-    var colunaRemover = $("<td>");
-    var link = $("<a>").addClass("botao-remover").attr("href", "#");
-    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
-
-    link.append(icone); // icone foi pra dentro do link
-    colunaRemover.append(link); // link foi pr dentro da colunm remove
-    linha.append(colunaUsuario); // coluna foi pra dentro de linha
-    linha.append(colunaPalavras); 
-    linha.append(colunaRemover);
-
-    return linha;
-}
-
-function removeLinha(event){
-
-        event.preventDefault();
-        $(this).parent().parent().remove();
-
-}
-
 
 function reiniciaJogo(timer){
     
