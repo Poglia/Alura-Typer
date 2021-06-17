7fd99ef909aa99ff6c6622b28a0045f6799bdf5f
Pedro Poglia <pedro.poglia@atua-suporte.1>
2021-06-17 15:21:07 -0300

    Criaçao das funçoes removeLinha() e novaLinha()
    novalinha cria uma linha na tabela que retorna um obj e remove linha pega o rebece o click no botao e apartir dele acessa a classe pai 2 vez ate chegar na linha completa e exclui-la

--- public/js/main.js
+++ public/js/main.js
@@ -2,15 +2,18 @@ const tempoInicial = $('#tempo-digitacao').text();
 var tempo = $('#tempo-digitacao');
 var campo = $(".campo-de-digitacao");
 var botaoReiniciar = $("#botao-reiniciar");
+var botaoDelete = $('.botao-remover');
 
 $(function() {
-    console.log("Pagina carregada");
+
+    //console.log("Pagina carregada");
     
     atualizaTamanhoFrase();
     atualizaDados();
     iniciaCronometro();
     inicializaMarcadores();
     botaoReiniciar.click(reiniciaJogo);
+    botaoDelete.click(removeLinha);
     
     
 });
@@ -22,8 +25,6 @@ function atualizaTamanhoFrase(){
     var tamanhoFrase = $('#tamanho-frase')
     tamanhoFrase.text(numeroPalavras);
 }
-
-
 
 function atualizaDados(){
 
@@ -38,6 +39,19 @@ function atualizaDados(){
         $('#cont-caracter').text(qtdCaracteres);
     });
 };
+
+function inserePlacar(){
+
+    var corpoTabela = $(".placar").find("tbody");
+    var numPalavras = $("#cont-palavra").text();
+    var usuario = "Pedro";
+
+    var linha = novaLinha(usuario,numPalavras);
+    
+    linha.find(".botao-remover").click(removeLinha);
+    corpoTabela.prepend(linha);
+
+}
 
 function inicializaMarcadores(){
 
@@ -77,27 +91,39 @@ function iniciaCronometro(){
             tempo.text(contador); // coloca o contador dentro da tag
             if(contador < 1 ){
                 finalizaJogo(timer);
+                inserePlacar();
                 //botaoReiniciar.attr("disabled", false);  
             }
 
         },1000)
+     
 
     });
 }
 
-function inserePlacar(){
+function novaLinha(usuario, palavras){
 
-    var corpoTabela = $(".placar").find("tbody");
-    var numPalavras = $("#cont-palavra").text();
-    var usuario = "Pedro";
-    
-    var linha = 
-    "<tr>" + 
-        "<td>" + usuario + "</td>" +
-        "<td>" + numPalavras +"</td>" +
-    "</tr>"
+    var linha = $("<tr>");
+    var colunaUsuario = $("<td>").text(usuario);
+    var colunaPalavras = $("<td>").text(palavras);
+    var colunaRemover = $("<td>");
+    var link = $("<a>").addClass("botao-remover").attr("href", "#");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
 
-    corpoTabela.append(linha);
+    link.append(icone); // icone foi pra dentro do link
+    colunaRemover.append(link); // link foi pr dentro da colunm remove
+    linha.append(colunaUsuario); // coluna foi pra dentro de linha
+    linha.append(colunaPalavras); 
+    linha.append(colunaRemover);
+
+    return linha;
+}
+
+function removeLinha(event){
+
+        event.preventDefault();
+        $(this).parent().parent().remove();
+
 }
 
 
@@ -117,6 +143,7 @@ function reiniciaJogo(timer){
         $('#cont-caracter').text('0');
 
         iniciaCronometro();
+
         campo.removeClass("campo-desativado");
         campo.removeClass("borda-vermelha");
         campo.removeClass("borda-verde");
@@ -127,5 +154,5 @@ function finalizaJogo(timer){
     campo.attr("disabled", true); //desabilita o campo
     clearInterval(timer); // trava o cronometro
     campo.addClass("campo-desativado");
-    inserePlacar();
+    
 }
